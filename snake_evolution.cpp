c8e857e3b0e794a8c51a48f214f1fd03acd10820
Mojtaba Movahedian <mojtabamovahedian@ugls6.ece.uvic.ca>
2023-07-27 08:13:07 -0700

    Fixed window freezing

--- snake_evolution.cpp
+++ snake_evolution.cpp
@@ -9,9 +9,22 @@ static void glfwErrorCallback(int id, const char* err)
   std::cerr << err <<"\n";
 }
 
+void keyCallback(GLFWwindow* window, int key, int scancode, int action, int mods)
+{
+    std::cout << "Key is: " << key << "\n";
+    if(key == GLFW_KEY_ESCAPE)
+    {
+        glfwSetWindowShouldClose(window, true);   
+    }
+}
+
+
+
 
 int main()
 {
+
+
     //set required opengl version
     glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 2);
     glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 1);
@@ -26,7 +39,10 @@ int main()
         std::cerr << "GLFW was not initialized. \n";
         return -1;
     }
-	GLFWwindow* window = glfwCreateWindow(800, 600, "Snake Evolution", NULL, NULL);
+
+
+
+	GLFWwindow* window = glfwCreateWindow(640, 480, "Snake Evolution", NULL, NULL);
 	glfwMakeContextCurrent(window);
     if(!window)
 	{
@@ -35,12 +51,35 @@ int main()
 		return -1;
 	}
 
+    //Key callback for reading user inputs
+    glfwSetKeyCallback(window, keyCallback);
+    
+    // std::cout << "GL version: " << glGetString(GL_VERSION) << std::endl;
+    // std::cout << "GL vendor : " << glGetString(GL_VENDOR  ) << std::endl;
+    // std::cout << "GL renderer: " << glGetString(GL_RENDERER ) << std::endl;
+    // std::cout << "GLSL version: " << glGetString(GL_SHADING_LANGUAGE_VERSION  ) << std::endl;
+
+
+    int width, height;
+    glfwGetFramebufferSize(window, &width, &height);
+    glViewport(0, 0, width, height);
+
+
+
+
     //running program
     while(!glfwWindowShouldClose(window))
     {
+        // glfwSetWindowShouldClose(window, true);    
+        // glClear(GL_COLOR_BUFFER_BIT);
+        glViewport(0, 0, width, height);
+        glClear(GL_COLOR_BUFFER_BIT);
+        glfwPollEvents();
 
     }
-
+  
+    //destroy window
+    glfwDestroyWindow(window);
 
     //clean up
     glfwTerminate();
